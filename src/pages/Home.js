8d319b4ccbbbf9f0a0ed1f5f6489d7aac902f4c0
sophakabuso <sopha.buso@gmail.com>
2023-09-04 14:16:34 +0200

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,39 +8,34 @@ import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div>
+    <div className={styles.container}>
       {/* Hero Section */}
       <div className={styles.heroSection} style={{ backgroundImage: `url(${backgroundImage})` }}>
-        <div className={styles.header}>
-          <div className={styles.links}>
-            <nav>
-            <Link to="/#facilities" className={styles.navLink}>Facilities</Link>
-
-              <Link to="/rooms" className={styles.navLink}>Rooms</Link>
-            </nav>
-          </div>
+        <div className={styles.cantainerlinks}>
+          <nav>
+            <Link to="/#facilities" className={styles.navLinkFacilties}>Facilities</Link>
+            <Link to="/rooms" className={styles.navLinkRooms}>Rooms</Link>
+          </nav>
         </div>
-
         <div className={styles.slogan}>
           <div className={styles.welcomeText}>
-            <p >Welcome to</p>
+            <p>Welcome to</p>
           </div>
           <div className={styles.hotelName}>
-            <h1 >SoB </h1>
+            <h1>SoB</h1>
           </div>
-          <div  className={styles.hotelTag}>
-            <h2>HOTELS </h2>
+          <div className={styles.hotelTag}>
+            <h2>HOTELS</h2>
           </div>
           <div className={styles.advText}>
-          <h2 >Book your stay and enjoy Luxury, redefined at the most affordable rates.</h2>
+            <h2>Book your stay and enjoy Luxury, redefined at the most affordable rates.</h2>
           </div>
         </div>
       </div>
-       <div id="facilities" className={styles.facilitiesContainer}>
-       <Facilities />
-       </div>
-      
-
+      {/* Facilities Container */}
+      <div id="facilities" className={styles.facilitiesContainer}>
+        <Facilities />
+      </div>
       {/* You can add more sections or content here */}
     </div>
   );
